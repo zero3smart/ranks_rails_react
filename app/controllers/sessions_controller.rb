90d168da572c89b23c867e4e5bec37d0b18f79fe
Daniel <calixtodaniel@gmail.com>
2018-09-06 04:45:56 -0300

    added auth_token to verify token route

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,8 +14,9 @@ class SessionsController < ApplicationController
 
   #Verifies the access_token so the client app would know if to login the user.
   def verify_token
-      #user = User.find_by(token: params[:auth_token])
-      user = User.find_by_auth_token!(request.headers[:token])
+      
+      #user = User.find_by_auth_token!(request.headers[:token])
+       user =  User.find_by(params[:auth_token])
     if user
 
       render json: {message:"verified",  status: 200}
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   #resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  post   'login'   => 'sessions#create', format: 'json'
+   post   'login'   => 'sessions#create'
     delete 'logout'  => 'sessions#destroy'
-    get    'verify'  => 'sessions#verify_token'
+    get    'verify/:auth_token'  => 'sessions#verify_token'
+    #get    'verify'  => 'sessions#verify_token'
     get    'forgot'  => 'password_resets#new'
     resources :users, param: :auth_token
     #resources :videos, param: :access_token
