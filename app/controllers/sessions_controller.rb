5f4ee04ffdc1d15b6415f26b7318e9635c6e00ee
Daniel <calixtodaniel@gmail.com>
2018-09-06 03:03:30 -0300

    check alow_token_to_be_used for user on RN token verification problem

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
 
   def create
     if user = User.validate_login(params[:email], params[:password])
-      allow_token_to_be_used_only_once_for(user)
+      #allow_token_to_be_used_only_once_for(user)
       send_token_for_valid_login_of(user)
     else
       render_unauthorized("Email and password combination are invalid")
