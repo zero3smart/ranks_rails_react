b5be11298320ed1d80471205772fef547323940f
Daniel <calixtodaniel@gmail.com>
2018-09-06 00:55:42 -0300

    added token verification to the session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,29 +8,43 @@ class SessionsController < ApplicationController
       send_token_for_valid_login_of(user)
     else
       render_unauthorized("Email and password combination are invalid")
-    
+
     end
   end
 
-  def destroy
-    logout
-    head :ok
+  #Verifies the access_token so the client app would know if to login the user.
+  def verify_access_token
+      user = User.find_by(auth_token: params[:auth_token])
+    if user
+
+      render json: {message:"verified",  status: 200}
+    else
+
+      render_unauthorized("Token failed verification")
+    end
   end
 
 
 
-  private
+def destroy
+  logout
+  head :ok
+end
 
-  def send_token_for_valid_login_of(user)
-    render json: {token: user.auth_token, status: 200}
-  end
 
-  def allow_token_to_be_used_only_once_for(user)
-    user.regenerate_auth_token
-  end
 
-  def logout
-    current_user.ivalidate_token
-  end
+private
+
+def send_token_for_valid_login_of(user)
+  render json: {token: user.auth_token, status: 200}
+end
+
+def allow_token_to_be_used_only_once_for(user)
+  user.regenerate_auth_token
+end
+
+def logout
+  current_user.ivalidate_token
+end
 
 end
