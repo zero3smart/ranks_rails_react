905c11e54ed36aef4d59833dd54ef99af8c8b168
Daniel <calixtodaniel@gmail.com>
2018-09-08 01:59:06 -0300

    check router verify without params

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
    post   'login'   => 'sessions#create'
     delete 'logout'  => 'sessions#destroy'
-    get    'verify/:auth_token'  => 'sessions#verify_token'
+    get    'verify'  => 'sessions#verify_token'
     
     get    'forgot'  => 'password_resets#new'
     resources :users, param: :auth_token
